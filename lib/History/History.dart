074b6f8ba1365fe682623cffc070b33c3c144145
Vinh Hoang <vinhhoang51@gmail.com>
2019-04-15 12:19:24 +0700

    lib\History\History.dart

---
+++ lib/History/History.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class History extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class _HistoryState extends State<History>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
